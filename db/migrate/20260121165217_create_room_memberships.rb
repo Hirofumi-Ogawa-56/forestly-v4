3f0f83b5d81828d72ad433da6fc68f17c4ddff8b
hirofumi.ogawa <hirofumi.ogawa@FONP00089.local>
2026-02-01 14:46:36 +0900

    Avoid duplicate table error for room_memberships

--- db/migrate/20260121165217_create_room_memberships.rb
+++ db/migrate/20260121165217_create_room_memberships.rb
@@ -1,5 +1,8 @@
 class CreateRoomMemberships < ActiveRecord::Migration[7.2]
   def change
+    # この1行を追加します！
+    return if table_exists?(:room_memberships)
+
     create_table :room_memberships do |t|
       t.references :profile, null: false, foreign_key: true
       t.references :chat_room, null: false, foreign_key: true
