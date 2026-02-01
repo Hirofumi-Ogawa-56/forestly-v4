95ff3c3251386137681bd647e0c5bbd7460cd689
hirofumi.ogawa <hirofumi.ogawa@FONP00089.local>
2026-02-01 14:39:33 +0900

    Fix activity migration to reference profiles table

--- db/migrate/20260119074558_create_activities.rb
+++ db/migrate/20260119074558_create_activities.rb
@@ -1,7 +1,10 @@
 class CreateActivities < ActiveRecord::Migration[7.2]
   def change
     create_table :activities do |t|
-      t.references :owner_profile, null: false, foreign_key: true
+      # foreign_key: true を削除、または
+      # もし profiles テーブルと紐付けたいなら以下の書き方に変更
+      t.references :owner_profile, null: false, foreign_key: { to_table: :profiles }
+      
       t.references :team, null: false, foreign_key: true
       t.string :title
       t.integer :status
